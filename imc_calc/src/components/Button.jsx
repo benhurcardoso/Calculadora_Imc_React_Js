4650bd5f51ac609a46084c107a0f15fc494c751e
Ben-hur Cardoso Mazarem <131083420+benhurcardoso@users.noreply.github.com>
2026-03-28 13:59:24 -0300

    style: estilização do formulário com flexbox e criação de componente Button
    Aplicado display flex para organização do layout.
    Criado componente reutilizável Button para ações de calcular e limpar.
    Melhorias visuais gerais no formulário.

---
+++ imc_calc/src/components/Button.jsx
@@ -0,0 +1,8 @@
+import './Button.css';
+
+const Button = ({ id, text }) => {
+  return <button id={id}>{text}</button>;
+  
+};
+
+export default Button;
--- imc_calc/src/components/ImcCalc.jsx
+++ imc_calc/src/components/ImcCalc.jsx
@@ -1,3 +1,4 @@
+import Button from "./Button";
 import "./ImcCalc.css";
 
 const ImcCalc = () => {
@@ -8,19 +9,19 @@ const ImcCalc = () => {
         <div className="form-inputs">
           <div className="form-control">
             <label htmlFor="height">Altura:</label>
-            <input type="text" name="height" id="height" placeholder="1,75" />
+            <input type="text" name="height" id="height" placeholder="Exemplo 1,75" />
           </div>
         </div>
         <div className="form-inputs">
           <div className="form-control">
             <label htmlFor="weight">Peso:</label>
-            <input type="text" name="weight" id="weight" placeholder="70.5" />
+            <input type="text" name="weight" id="weight" placeholder="Exemplo 70.5" />
           </div>
         </div>
 
         <div className="action-control">
-            <button>Calcular</button>
-            <button>Limpar</button>
+            <Button id= "calc-btn" text="Calcular" />
+            <Button id="clear-btn" text="Limpar" />
         </div>
       </form>
     </div>
